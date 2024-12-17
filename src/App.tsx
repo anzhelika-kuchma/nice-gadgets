1a9deb84d37dbf84a2c44344d31d3fdcd5cc0bab
anzhelikakuchma <anzhelika.kuchma@gmail.com>
2024-12-17 19:15:51 -0330

    project skeleton

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import { Layout } from './Layout';
+import { HomePage } from './modules/HomePage';
+import { Phones } from './modules/Catalog/Phones';
+import { Tablets } from './modules/Catalog/Tablets';
+import { Accessories } from './modules/Catalog/Accessories';
+import { ProductDetails } from './modules/ProductDetails';
+import { Favorites } from './modules/Favorites';
+import { Cart } from './modules/Cart';
+import { ErrorPage } from './modules/ErrorPage';
 
-function App() {
-  const [count, setCount] = useState(0)
+import './App.scss';
 
+export const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<HomePage />} />
+        <Route path="/phones" element={<Phones />} />
+        <Route path="/phones/:productId" element={<ProductDetails />} />
+        <Route path="/tablets" element={<Tablets />} />
+        <Route path="/tablets/:productId" element={<ProductDetails />} />
+        <Route path="/accessories" element={<Accessories />} />
+        <Route path="/accessories/:productId" element={<ProductDetails />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/favorites" element={<Favorites />} />
+        <Route path="/favorites/:productId" element={<ProductDetails />} />
+        <Route path="*" element={<ErrorPage />} />
+      </Route>
+    </Routes>
+  );
+};
